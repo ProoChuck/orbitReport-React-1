23fdefe118bd5fb30197fcf959b12099c83d1ffa
ProoChuck <rssnow58@gmail.com>
2023-08-11 18:39:10 -0500

    made changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
+import { useState } from 'react';
+import satData from "./components/satData";
 
 
 function App() {
-  return (
-    <div>
-      <Buttons />
-      <Table />
-    </div>
+    const[sat, setSat] = useState(satData);
+    const dispaySats = [...new Set(satData.map((data) => data.orbitType))];
+    const filterByType = (currentType) => {
+        const displaySats = satData.filter((newSatDisplay) => {
+            return newSatDisplay.orbitType === currentType;
+        });
+        setSat(displaySats);
+    };
+    return(
+    <>
+      <Banner/>
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={dispaySats} 
+      />
+      <Table sat={sat}/>
+    </>
   );
 }
 
